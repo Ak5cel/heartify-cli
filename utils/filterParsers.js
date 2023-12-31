d8e1829c22b5aba4b9cec8f419deab6a0b55e7dd
Ak5cel <akhilaficel@gmail.com>
2023-12-31 22:15:57 +0530

    refactor filter parser

--- utils/filterParsers.js
+++ utils/filterParsers.js
@@ -2,28 +2,7 @@ const { InvalidArgumentError } = require("commander");
 const { parseDateFrom, parseDateTo } = require("./dateTimeParsers");
 
 exports.parseFilters = (str, previous) => {
-  const validFormat = /^.*\=.*$/; // string with at least one equal sign in the middle to split on
-  if (!validFormat.test(str)) {
-    throw new InvalidArgumentError("Filter must be of the format field=value");
-  }
-
-  let [field, val] = str.split("=");
-
-  if (field) {
-    field = field.trim();
-  }
-  if (val) {
-    val = val.trim();
-  }
-
-  // after trimming, check field and value are non-empty
-  if (!field) {
-    throw new InvalidArgumentError("Field name must be non-empty");
-  }
-
-  if (!val) {
-    throw new InvalidArgumentError("Field value must be non-empty");
-  }
+  let { field, val } = _extractFieldValuePair(str);
 
   const strFields = ["artist"];
   const dateTimeFields = ["released_on"];
@@ -51,41 +30,9 @@ exports.parseFilters = (str, previous) => {
     throw new InvalidArgumentError(`Unknown filter field: ${field}`);
   }
 
-  // rangeFormat = /^\[.*\,.*\]$/;
-  const rangeFormat = /^\[(?<from>.*)\,(?<to>.*)\]$/;
-  const squareBrackets = /^\[.*\]$/;
-
-  if (squareBrackets.test(val) && !rangeFormat.test(val)) {
-    throw new InvalidArgumentError("Range must be of the format [from,to]");
-  }
-
-  if (rangeFormat.test(val)) {
-    const matches = val.match(rangeFormat);
-    let { from, to } = matches.groups;
-
-    if (from.length) {
-      from = from.trim();
-    }
-    if (to.length) {
-      to = to.trim();
-    }
-
-    val = {};
-
-    if (from.length) {
-      val.from = from;
-    }
-
-    if (to.length) {
-      val.to = to;
-    }
-
-    if (!from.length && !to.length) {
-      throw new InvalidArgumentError(
-        "Range must contain either a `from` value or `to` value"
-      );
-    }
-
+  if (typeof val === "object") {
+    // if val is an object, it will have a `from` and `to` key
+    // it could be either a DateTime field or a Number field
     if (dateTimeFields.includes(field)) {
       if (val.from) val.from = parseDateFrom(val.from);
       if (val.to) val.to = parseDateTo(val.to);
@@ -134,6 +81,64 @@ exports.parseFilters = (str, previous) => {
   return previous;
 };
 
-const _extractFilters = (str) => {};
+const _extractFieldValuePair = (str) => {
+  const validFormat = /^.*\=.*$/; // string with at least one equal sign in the middle to split on
+  if (!validFormat.test(str)) {
+    throw new InvalidArgumentError("Filter must be of the format field=value");
+  }
 
-const _filterDatatype = (str) => {};
+  let [field, val] = str.split("=");
+
+  if (field) {
+    field = field.trim();
+  }
+  if (val) {
+    val = val.trim();
+  }
+
+  // after trimming, check field and value are non-empty
+  if (!field) {
+    throw new InvalidArgumentError("Field name must be non-empty");
+  }
+
+  if (!val) {
+    throw new InvalidArgumentError("Field value must be non-empty");
+  }
+
+  const rangeFormat = /^\[(?<from>.*)\,(?<to>.*)\]$/;
+  const squareBrackets = /^\[.*\]$/;
+
+  if (squareBrackets.test(val) && !rangeFormat.test(val)) {
+    throw new InvalidArgumentError("Range must be of the format [from,to]");
+  }
+
+  if (rangeFormat.test(val)) {
+    const matches = val.match(rangeFormat);
+    let { from, to } = matches.groups;
+
+    if (from.length) {
+      from = from.trim();
+    }
+    if (to.length) {
+      to = to.trim();
+    }
+
+    val = {};
+
+    if (from.length) {
+      val.from = from;
+    }
+
+    if (to.length) {
+      val.to = to;
+    }
+
+    if (!from.length && !to.length) {
+      throw new InvalidArgumentError(
+        "Range must contain either a `from` value or `to` value"
+      );
+    }
+  }
+
+  return { field, val };
+};
