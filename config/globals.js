2676c9b421c2f8bd379d9b84fab2607681ef61b3
Ak5cel <akhilaficel@gmail.com>
2024-01-01 11:18:15 +0530

    remove unused scopes

--- config/globals.js
+++ config/globals.js
@@ -2,12 +2,9 @@ const config = {
   SPOTIFY_CLIENT_ID: "7bcaae5e99d54f378d1c34a0a9ae015f",
   SPOTIFY_SAVED_TRACKS: "https://api.spotify.com/v1/me/tracks",
   scopes: [
-    "playlist-read-private",
     "playlist-modify-private",
     "playlist-modify-public",
-    "user-library-modify",
     "user-library-read",
-    "user-read-private",
   ],
   redirect_uri: "http://localhost:9090/callback",
 };
